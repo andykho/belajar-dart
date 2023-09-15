e379c01b3802b6d2d82b8d4a549c82329f3cde83
andykho <andykho52@gmail.com>
2023-09-15 10:47:18 +0700

    gppbang

---
+++ bin/tutor_dart.dart
@@ -0,0 +1,5 @@
+import 'package:tutor_dart/tutor_dart.dart' as tutor_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tutor_dart.calculate()}!');
+}
---
+++ test/tutor_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tutor_dart/tutor_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
